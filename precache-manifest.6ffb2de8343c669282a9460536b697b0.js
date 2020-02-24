183a12021cda1ca3ba8b8cc12a40e3874414ba73
jeremy1 <jeremy1@uni-bremen.de>
2020-02-24 18:00:50 +0100

    Updates

---
+++ precache-manifest.6ffb2de8343c669282a9460536b697b0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "abb4f2e600af004d8acebd802c8e9071",
+    "url": "/poddy-web/index.html"
+  },
+  {
+    "revision": "b97d1e7dc8c7aec0cfe0",
+    "url": "/poddy-web/static/css/2.70d9d258.chunk.css"
+  },
+  {
+    "revision": "35d2b9260452af90354b",
+    "url": "/poddy-web/static/css/main.29344b91.chunk.css"
+  },
+  {
+    "revision": "b97d1e7dc8c7aec0cfe0",
+    "url": "/poddy-web/static/js/2.1fd64ada.chunk.js"
+  },
+  {
+    "revision": "496630de2713ba70683da5dc3fc3c1a4",
+    "url": "/poddy-web/static/js/2.1fd64ada.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "35d2b9260452af90354b",
+    "url": "/poddy-web/static/js/main.1ba5f988.chunk.js"
+  },
+  {
+    "revision": "c05dedd4cf2f971df17a",
+    "url": "/poddy-web/static/js/runtime-main.e80ac9b8.js"
+  }
+]);
